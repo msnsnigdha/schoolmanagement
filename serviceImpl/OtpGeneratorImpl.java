2db3ce582e2564012235f55aa810f9c77e06a1d9
msnsnigdha <154515275+msnsnigdha@users.noreply.github.com>
2023-12-29 10:31:56 +0530

    Update OtpGeneratorImpl.java

--- serviceImpl/OtpGeneratorImpl.java
+++ serviceImpl/OtpGeneratorImpl.java
@@ -1,20 +1,20 @@
-package com.example.PROJECT.serviceImpl;
+// package com.example.PROJECT.serviceImpl;
 
-import java.util.Random;
+// import java.util.Random;
 
-public class OtpGeneratorImpl 
-{
-	private static final int OTP_LENGTH = 6;
+// public class OtpGeneratorImpl 
+// {
+// 	private static final int OTP_LENGTH = 6;
 
-	public static String generateOtp() {
-		Random random = new Random();
-		StringBuilder otp = new StringBuilder();
+// 	public static String generateOtp() {
+// 		Random random = new Random();
+// 		StringBuilder otp = new StringBuilder();
 
-		for (int ctr = 0; ctr < OTP_LENGTH; ctr++) {
-			otp.append(random.nextInt(10));
-		}
+// 		for (int ctr = 0; ctr < OTP_LENGTH; ctr++) {
+// 			otp.append(random.nextInt(10));
+// 		}
 
-		return otp.toString();
-	}
-}
+// 		return otp.toString();
+// 	}
+// }
 
